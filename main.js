3ed02eb4edfd35c2f2f27ca7a7bbe89db51eec61
Kouhei Ho <alan829221@gmail.com>
2023-09-25 01:19:19 +0800

    UPDATE: change map

--- main.js
+++ main.js
@@ -43,15 +43,26 @@ tempEl.addEventListener("click", () => {
 // ---- Leaflet 初始化 ----
 const map = L.map("map").setView([23.6978, 120.9605], 8);
 
-const Stadia_AlidadeSmoothDark = L.tileLayer(
-  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+// const Stadia_AlidadeSmoothDark = L.tileLayer(
+//   "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+//   {
+//     maxZoom: 20,
+//     attribution:
+//       '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+//   }
+// );
+// Stadia_AlidadeSmoothDark.addTo(map);
+
+const defaultMap = L.tileLayer(
+  "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
   {
-    maxZoom: 20,
+    maxZoom: 19,
     attribution:
-      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }
 );
-Stadia_AlidadeSmoothDark.addTo(map);
+
+defaultMap.addTo(map);
 // ---- Leaflet 初始化 ----
 
 // ---- Layer ----
